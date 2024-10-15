22e804bbb46e3d4210f5eca629b00e505c307c9b
Adrian <adrianfernandezgalan@gmail.com>
2024-10-15 15:22:18 +0200

    CSV de entrada y de salida de incidencias_usuario por argumento

--- entregable2/pyscripts/preproceso_incidencias_usuario.py
+++ entregable2/pyscripts/preproceso_incidencias_usuario.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-def preproceso_incidencias_usuario():
-    df = pd.read_csv("../csvs/IncidenciasUsuariosSucio.csv")
+def preproceso_incidencias_usuario(csv_input, cvs_output):
+    df = pd.read_csv(csv_input)
     df["TIPO_INCIDENCIA"] = df["TIPO_INCIDENCIA"].str.upper()
     df["FECHA_REPORTE"] = pd.to_datetime(df["FECHA_REPORTE"], format="mixed", dayfirst=True).dt.strftime("%d-%m-%Y")
     df["ESTADO"] = df["ESTADO"].str.upper()
 
+    df.to_csv(cvs_output)
+    
 preproceso_incidencias_usuario()
