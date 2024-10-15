a03dc5b9424da7b9468bd6a1d846101d05f2dc3e
Adrian <adrianfernandezgalan@gmail.com>
2024-10-15 15:36:33 +0200

    Ahora sí

--- entregable2/pyscripts/preproceso_incidencias_usuario.py
+++ entregable2/pyscripts/preproceso_incidencias_usuario.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def preproceso_incidencias_usuario(csv_input, cvs_output):
+    csv_input = csv_input + "EncuestasSatisfaccionSucio.csv"
+    csv_output = csv_output + "encuestas_satisfaccion_limpio.csv"
     df = pd.read_csv(csv_input)
     df["TIPO_INCIDENCIA"] = df["TIPO_INCIDENCIA"].str.upper()
     df["FECHA_REPORTE"] = pd.to_datetime(df["FECHA_REPORTE"], format="mixed", dayfirst=True).dt.strftime("%d-%m-%Y")
