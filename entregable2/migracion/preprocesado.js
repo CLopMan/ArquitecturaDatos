34882f6f0bcdf4f4171371e12d8de0faa21d77c4
CLopMan <cesarlopman@gmail.com>
2024-11-05 16:05:39 +0100

    reestructura y preprocesado

---
+++ entregable2/migracion/preprocesado.js
@@ -0,0 +1,322 @@
+use entregable2_aux;
+// ---------------- CAST --------------
+
+db.areas.aggregate([
+    {
+        $addFields: {
+            _id: {
+                $convert: {
+                    input: "$_id",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields:{
+            LATITUD: {
+                $convert: {
+                    input:"$LATITUD",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            },
+            LONGITUD: {
+                $convert: {
+                    input:"$LONGITUD",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            },
+            NUM_VIA: {
+                $convert: {
+                    input:"$NUM_VIA",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields: {
+            FECHA_INSTALACION: {
+                $dateFromString: {
+                    dateString: "$FECHA_INSTALACION",
+                    format: "%Y-%m-%dT%H:%M:%SZ"
+                }
+            }
+        }
+    },
+    {
+        $out: {
+            db: "entregable2_aux",
+            coll: "areas"
+        }
+    }
+]);
+
+db.juegos.aggregate([
+    {
+        $addFields: {
+            _id: {
+                $convert: {
+                    input: "$_id",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields:{
+            LATITUD: {
+                $convert: {
+                    input:"$LATITUD",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            },
+            LONGITUD: {
+                $convert: {
+                    input:"$LONGITUD",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            },
+            NUM_VIA: {
+                $convert: {
+                    input:"$NUM_VIA",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields: {
+            FECHA_INSTALACION: {
+                $dateFromString: {
+                    dateString: "$FECHA_INSTALACION",
+                    format: "%Y-%m-%dT%H:%M:%SZ"
+                }
+            }
+        }
+    },
+    {$out: {
+        db: "entregable2_aux", coll:"juegos"
+    }}
+]);
+
+db.encuestas_satisfaccion.aggregate([
+    {
+        $addFields: {
+            _id: {
+                $convert: {
+                    input: "$_id",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields: {
+            FECHA: {
+                $dateFromString: {
+                    dateString: "$FECHA",
+                    format: "%Y-%m-%dT%H:%M:%SZ"
+                }
+            }
+        } 
+    },
+    {$out: {
+        db: "entregable2_aux", coll:"encuestas_satisfaccion"
+    }}
+]);
+
+db.incidentes_seguridad.aggregate([
+    {
+        $addFields: {
+            _id: {
+                $convert: {
+                    input: "$_id",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields: {
+            FECHA_REPORTE: {
+                $dateFromString: {
+                    dateString: "$FECHA_REPORTE",
+                    format: "%Y-%m-%dT%H:%M:%SZ"
+                }
+            }
+        } 
+    },
+    {$out: {
+        db: "entregable2_aux", coll:"encuestas_satisfaccion"
+    }}
+]);
+
+db.usuarios.aggregate([
+    {
+        $addFields: {
+            _id: {
+                $convert: {
+                    input: "$_id",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $out: {
+            db: "entregable2_aux",
+            coll: "usuarios"
+        }
+    }
+])
+
+db.mantenimiento.aggregate([
+    {
+        $addFields: {
+            FECHA_REPORTE: {
+                $dateFromString: {
+                    dateString: "$FECHA_REPORTE",
+                    format: "%Y-%m-%dT%H:%M:%SZ"
+                }
+            }
+        } 
+    },
+    {
+        $out: {
+            db: "entregable2_aux",
+            coll: "usuarios"
+        }
+    }
+]);
+
+db.incidentes_seguridad.aggregate([
+    {
+        $addFields: {
+            _id: {
+                $convert: {
+                    input: "$_id",
+                    to: "string",
+                    onError: null,
+                    onNull: null
+                }
+            }
+        }
+    },
+    {
+        $addFields: {
+            FECHA_REPORTE: {
+                $dateFromString: {
+                    dateString: "$FECHA_REPORTE",
+                    format: "%Y-%m-%dT%H:%M:%SZ"
+                }
+            }
+        } 
+    },
+    {$out: {
+        db: "entregable2_aux", coll:"encuestas_satisfaccion"
+    }}
+]);
+
+// ------------- PARSER -----------------
+
+db.incidencias_usuarios.aggregate([
+    {
+        // Convertir el string 'UsuarioID' en un array de valores
+        $addFields: {
+            MantenimientoID: {
+                $split: [
+                    {
+                        $replaceAll: {
+                            input: {
+                                $replaceAll: {
+                                    input: {
+                                        $replaceAll: {
+                                            input:
+                                            {
+                                                $replaceAll: {
+                                                    input: "$MantenimeintoID",
+                                                    find: " ",
+                                                    replacement: ""
+                                                }
+                                            },
+                                            find: "'",
+                                            replacement: ""
+                                        }
+                                    },
+                                    find: "]",
+                                    replacement: ""
+                                }
+                            },
+                            find: "[",
+                            replacement: ""
+                        }
+                    }, ","]
+            },
+            UsuarioID: {
+                $split: [
+                    {
+                        $replaceAll: {
+                            input: {
+                                $replaceAll: {
+                                    input: {
+                                        $replaceAll: {
+                                            input:
+                                            {
+                                                $replaceAll: {
+                                                    input: "$UsuarioID",
+                                                    find: " ",
+                                                    replacement: ""
+                                                }
+                                            },
+                                            find: "'",
+                                            replacement: ""
+                                        }
+                                    },
+                                    find: "]",
+                                    replacement: ""
+                                }
+                            },
+                            find: "[",
+                            replacement: ""
+                        }
+                    }, ","]
+            }
+        }
+    },
+    {
+        $project: {
+            _id: 1,
+            TIPO_INCIDENCIA: 1,
+            FECHA_REPORTE: 1,
+            ESTADO: 1,
+            UsuarioID: 1,
+            MantenimientoID: 1
+        }
+    },
+    {
+        $out: { db: "entregable2", coll: "incidencias_usuarios" }
+    }
+]);
