73f32149d0503e32157f29510a2cc9bbc21cdfb1
CLopMan <cesarlopman@gmail.com>
2024-11-07 11:57:28 +0100

    correccion de encuestas de usuario

--- entregable2/migracion/preprocesado.js
+++ entregable2/migracion/preprocesado.js
@@ -185,13 +185,13 @@ db.encuestas_satisfaccion.aggregate([
     },
     {
         $addFields: {
-            fecha: {
+            FECHA: {
                 $dateFromString: {
-                    dateString: "$fecha",
+                    dateString: "$FECHA",
                     format: "%Y-%m-%dT%H:%M:%SZ"
                 }
             }
-        }
+        } 
     },
     {
         $addFields: {
@@ -203,13 +203,11 @@ db.encuestas_satisfaccion.aggregate([
                     onNull: null
                 }
             }
-        }
+        } 
     },
-    {
-        $out: {
-            db: "entregable2_old", coll: "encuestas_satisfaccion"
-        }
-    }
+    {$out: {
+        db: "entregable2_old", coll:"encuestas_satisfaccion"
+    }}
 ]);
 
 db.incidentes_seguridad.aggregate([
@@ -233,13 +231,11 @@ db.incidentes_seguridad.aggregate([
                     format: "%Y-%m-%dT%H:%M:%SZ"
                 }
             }
-        }
+        } 
     },
-    {
-        $out: {
-            db: "entregable2_old", coll: "encuestas_satisfaccion"
-        }
-    }
+    {$out: {
+        db: "entregable2_old", coll:"incidentes_seguridad"
+    }}
 ]);
 
 db.usuarios.aggregate([
@@ -272,46 +268,15 @@ db.mantenimiento.aggregate([
                     format: "%Y-%m-%dT%H:%M:%SZ"
                 }
             }
-        }
+        } 
     },
     {
         $out: {
             db: "entregable2_old",
-            coll: "usuarios"
+            coll: "mantenimiento"
         }
     }
 ]);
-
-db.incidentes_seguridad.aggregate([
-    {
-        $addFields: {
-            _id: {
-                $convert: {
-                    input: "$_id",
-                    to: "string",
-                    onError: null,
-                    onNull: null
-                }
-            }
-        }
-    },
-    {
-        $addFields: {
-            FECHA_REPORTE: {
-                $dateFromString: {
-                    dateString: "$FECHA_REPORTE",
-                    format: "%Y-%m-%dT%H:%M:%SZ"
-                }
-            }
-        }
-    },
-    {
-        $out: {
-            db: "entregable2_old", coll: "encuestas_satisfaccion"
-        }
-    }
-]);
-
 // ------------- PARSER -----------------
 
 db.incidencias_usuarios.aggregate([
