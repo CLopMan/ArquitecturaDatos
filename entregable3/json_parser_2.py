f40e49b102c4d4f45a1b46cdcac401dfa13d8c21
manu <100472310@alumnos.uc3m.es>
2024-11-26 14:14:08 +0100

    parser actualizado

--- entregable3/json_parser_2.py
+++ entregable3/json_parser_2.py
@@ -1,13 +1,12 @@
 import re
 
-count_keys = 0
-
 with open("sample.json", "r", encoding="ISO-8859-1") as f:
     file_content = f.read()
     file_parsed = re.sub(r"(?<!}\s)\n(?!\s{)", "", file_content)
     file_parsed = re.sub(r" +", " ", file_parsed)
-    file_parsed = "[" + file_parsed + "]"
+    file_parsed = file_parsed + "]"
     file_parsed = re.sub(r"\n(?!\s])", ",\n", file_parsed)
+    file_parsed = "[\n" + file_parsed
 
 with open("sample_parsed.json", "w", encoding="ISO-8859-1") as f:
     f.write(file_parsed)
